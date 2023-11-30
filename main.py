6b6dc90379bc38a0d45e0a8f728eb5f49be03233
Dimmed Light <113694089+dimmed-light@users.noreply.github.com>
2023-11-30 14:01:15 +0200

    minor adjustments

---
+++ main.py
@@ -0,0 +1,13 @@
+import webcrawler
+
+def main():
+    
+    # TODO: add proper input handling from a web server (flask)
+    webcrawler.pubmed_crawl('covid')
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
--- webcrawler.py
+++ webcrawler.py
@@ -2,37 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 
 class Publication:
-    def __init__(self, title, authors, abstract, date):
-        self.title = title
-        self.authors = authors
-        self.abstract = abstract
-        self.date = date
-
+    title = str()
+    authors = str()
+    abstract = str()
+    date = str()
+    
     def __str__(self):
         return f"{self.title}, {self.authors}, {self.abstract}, {self.date}"
 
-# TODO: add proper input handling from a web server (flask)
-# same input
-query = 'covid'
+def pubmed_crawl(query: str) -> list[Publication]:
+    #query = 'covid'
 
-# process it so that it can be used with PubMed
-query.replace(' ', '+')
+    # process it so that it can be used with PubMed
+    query.replace(' ', '+')
 
-# search and get response
-response = requests.get('https://pubmed.ncbi.nlm.nih.gov/?term=' + query)
+    # search and get response
+    response = requests.get('https://pubmed.ncbi.nlm.nih.gov/?term=' + query)
 
-# parse HTML
-soup = BeautifulSoup(response.text, 'html.parser')
+    # parse HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# get content
-results = soup.find_all("div", { "class": "docsum-content" })
+    # get content
+    results = soup.find_all("div", { "class": "docsum-content" })
 
-
-publications = []
-for result in results:
-    title = result.find('a').text.strip()
-    print(title)
-    # authors =
-    # ... 
+    publications = []
+    for result in results:
+        publication = Publication()
+        publication.title = result.find('a').text.strip()
+        
+        print(publication)
+        # authors =
+        # ...
+        publications.append(publication) 
     
-#print(results[0])
+    return publications
